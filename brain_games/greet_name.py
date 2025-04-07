23a6ef7030e90fc7ed7f526890e47e66bff43a9a
A-leks-andr <calllamurzakov@gmail.com>
2025-04-07 21:22:40 +0300

    add func greet

---
+++ brain_games/greet_name.py
@@ -0,0 +1,10 @@
+import prompt
+
+
+def greet_and_name():
+
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
+    return name
+
