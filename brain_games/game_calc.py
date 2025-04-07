b8a695b1fc9a26ee7c4dd97e6e99594a5c2ebccd
A-leks-andr <calllamurzakov@gmail.com>
2025-04-07 21:23:17 +0300

    add calc

---
+++ brain_games/game_calc.py
@@ -0,0 +1,30 @@
+import operator
+import prompt
+from random import randint
+
+
+def get_calc(name):
+
+    act = [operator.add, operator.sub, operator.mul]
+    sim = ['+', '-', '*']
+    print('What is the result of the expression?')
+    for i in range(3):
+
+        a = randint(1, 15)
+        b = randint(1, 15)
+        check = str(act[i](a, b))
+        print(f'Question: {a} {sim[i]} {b}')
+        answer = prompt.string('Your answer: ')
+
+        if answer == check:
+            print('Correct')
+
+        else:
+            print(f'"{answer}" is wrong answer ;(.'
+                   'Correct answer was "{check}".')
+            print(f"Let's try again, {name}")
+            break
+
+    else:
+        print(f'Congratulations, {name}')
+
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,13 @@
+from brain_games.greet_name import greet_and_name
+from brain_games.game_calc import get_calc
+
+
+def main():
+
+    name = greet_and_name()
+    get_calc(name)
+
+if __name__ == '__main__':
+
+
+    main()
