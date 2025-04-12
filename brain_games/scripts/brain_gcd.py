297e3dc09d665699907a22b9e2ea6b66cbcb4604
A-leks-andr <calllamurzakov@gmail.com>
2025-04-12 20:27:33 +0300

    change brain_gcd

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,13 +1,13 @@
-from brain_games.greet_name import greet_and_name
 from brain_games.game_gcd import get_gcd
+from brain_games.greet_name import greet_and_name
 
 
 def main():
 
     name = greet_and_name()
-    get_gcd
+    get_gcd(name)
+
 
 if __name__ == '__main__':
-
 
     main()
