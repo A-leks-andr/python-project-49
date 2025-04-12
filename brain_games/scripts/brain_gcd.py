9fe238647172ba0efa7827ed3ca2b4d06f54b44d
A-leks-andr <calllamurzakov@gmail.com>
2025-04-12 20:11:04 +0300

    add brain_gcd

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,13 @@
+from brain_games.greet_name import greet_and_name
+from brain_games.game_gcd import get_gcd
+
+
+def main():
+
+    name = greet_and_name()
+    get_gcd
+
+if __name__ == '__main__':
+
+
+    main()
