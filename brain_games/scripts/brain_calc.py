15085c331fd76bf6af53a8b20fb5bb7dd61a74fc
A-leks-andr <calllamurzakov@gmail.com>
2025-04-14 01:19:09 +0300

    change file to ruff

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,12 @@
-from brain_games.greet_name import greet_and_name
 from brain_games.game_calc import get_calc
+from brain_games.greet_name import greet_and_name
 
 
 def main():
 
     name = greet_and_name()
     get_calc(name)
+
 
 if __name__ == '__main__':
 
