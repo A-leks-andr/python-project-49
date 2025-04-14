045f7ba42a37173d2718137e353188fe1e5ba92d
A-leks-andr <calllamurzakov@gmail.com>
2025-04-14 05:35:04 +0300

    change file game_prime

--- brain_games/game_prime.py
+++ brain_games/game_prime.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randrange
 
 import prompt
 
@@ -8,7 +8,7 @@ def is_prime(name):
     prime_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
                   47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103]
     for _ in range(3):
-        number = randint(1, 103)
+        number = randrange(1, 106, 2)
         if number in prime_list:
             check = 'yes'
         else:
