c6845690f996ea0295ca413aea8c34f76cc800d9
A-leks-andr <calllamurzakov@gmail.com>
2025-04-14 05:21:34 +0300

    add file brain_prime

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,13 @@
+from brain_games.game_prime import is_prime
+from brain_games.greet_name import greet_and_name
+
+
+def main():
+
+    name = greet_and_name()
+    is_prime(name)
+
+
+if __name__ == '__main__':
+
+    main()
