e19797ed2cf1998781a4b62a94cb4b61ca7ed436
A-leks-andr <calllamurzakov@gmail.com>
2025-04-17 22:14:56 +0300

    change 6 files ruff format

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,11 @@
-from brain_games.game_is_evens import is_evens
+from brain_games.game_is_evens import which_evens
 from brain_games.greet_name import greet_and_name
 
 
 def main():
     name = greet_and_name()
-    is_evens(name)
+    which_evens(name)
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     main()
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -3,11 +3,9 @@ from brain_games.greet_name import greet_and_name
 
 
 def main():
-
     name = greet_and_name()
     get_progression(name)
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     main()
