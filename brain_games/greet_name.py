de45479190a5b249746cb20aa7771c6bbb936400
A-leks-andr <calllamurzakov@gmail.com>
2025-04-17 22:15:43 +0300

    change 3 files ruff format

--- brain_games/greet_name.py
+++ brain_games/greet_name.py
@@ -2,9 +2,7 @@ import prompt
 
 
 def greet_and_name():
-
     print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
     return name
-
